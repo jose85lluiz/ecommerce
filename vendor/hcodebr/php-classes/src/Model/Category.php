d0404d044cb0e5a1a7e4e14238feaa8fefaf07cc
joseluiz <jose--luiz@hotmail.com>
2022-08-03 14:31:40 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -29,7 +29,7 @@ $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)",
 ));
 
 $this->setData($results[0]);
-
+Category::updateFile();
 }
 
 
@@ -44,6 +44,7 @@ $this ->setData($results[0]);
 
 }
 
+
 public function delete(){
 
 $sql = new sql();
@@ -51,9 +52,24 @@ $sql ->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
   ":idcategory" => $this->getidcategory()
  
 ]);
-
-
+   Category::updateFile();
+ 
 }
+
+
+public static function updateFile()
+{
+
+$categories = Category::listAll();
+
+$html =[];
+foreach ($categories as $row) {
+	array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+
+  }
+  file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('',$html));
+
+  }
 
 
 }
